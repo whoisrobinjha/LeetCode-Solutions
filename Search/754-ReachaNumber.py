b8dfe8b94b321c967e3dfda16bd8f43419644b44
whoisrobinjha <robinjha370@gmail.com>
2023-03-01 11:05:11 +0530

    solution 754 modified

--- Search/754-ReachaNumber.py
+++ Search/754-ReachaNumber.py
@@ -2,15 +2,18 @@
 #https://leetcode.com/problems/reach-a-number/
 
 class Solution:
-    def reachNumber(self, target: int) -> int:
+    def reachNumber(self, target):
         target = abs(target)
         start = 0
         end = float('inf')
         while start < end:
             mid = (start + end) >> 1
-            total_distance = mid * (mid + 1) // 2
+            total_distance = mid * (mid + 1) >> 1
             if total_distance >= target and (total_distance - target) % 2 == 0:
                 end = mid
             else:
                 start = mid + 1
         return start
+    
+target = 2
+print(Solution().reachNumber(target))
