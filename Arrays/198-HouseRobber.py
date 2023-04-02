b167c293f71129ad64cb8834f774079b18b7cfcf
whoisrobinjha <robinjha370@gmail.com>
2023-04-02 21:59:16 +0530

    solution to problem 198 modified

--- Arrays/198-HouseRobber.py
+++ Arrays/198-HouseRobber.py
@@ -3,18 +3,17 @@
 
 class Solution:
     def rob(self, nums):
-        n = len(nums)
-        if n == 0:
-            return 0
-        elif n == 1:
-            return nums[0]
-        else:
-            dp = [0] * n
-            dp[0] = nums[0]
-            dp[1] = max(nums[0], nums[1])
-            for i in range(2, n):
-                dp[i] = max(dp[i-1], dp[i-2] + nums[i])
-            return dp[n-1]
+        robbed_yes = 0
+        robbed_no = 0
+        
+        for i in range(len(nums)):
+            current_robbed = robbed_no + nums[i]
+            current_not_robbed = max(robbed_yes, robbed_no)
+            
+            robbed_yes = current_robbed
+            robbed_no = current_not_robbed
+            
+        return max(robbed_yes, robbed_no)
     
 nums = [1,2,3,1]
 print(Solution().rob(nums))
