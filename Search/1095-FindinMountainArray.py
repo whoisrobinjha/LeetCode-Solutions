af1f2f0e928a4375cfa2806366bd9e2010f6a0d1
whoisrobinjha <robinjha370@gmail.com>
2023-04-11 12:00:53 +0530

    solution to problem 1095 added

---
+++ Search/1095-FindinMountainArray.py
@@ -0,0 +1,44 @@
+#LeetCode Problem 1095 - Find in Mountain Array Solution
+# https://leetcode.com/problems/find-in-mountain-array/
+
+# """
+# This is MountainArray's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class MountainArray:
+#    def get(self, index: int) -> int:
+#    def length(self) -> int:
+
+class Solution:
+    def findInMountainArray(self, target, mountain_arr):
+        n = mountain_arr.length()
+        l, r = 0, n - 1
+        while l < r:
+            mid = (l + r) // 2
+            if mountain_arr.get(mid) < mountain_arr.get(mid + 1):
+                l = mid + 1
+            else:
+                r = mid
+        peak = l
+
+        l, r = 0, peak
+        while l <= r:
+            mid = (l + r) // 2
+            if mountain_arr.get(mid) == target:
+                return mid
+            elif mountain_arr.get(mid) < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+
+        l, r = peak, n - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if mountain_arr.get(mid) == target:
+                return mid
+            elif mountain_arr.get(mid) < target:
+                r = mid - 1
+            else:
+                l = mid + 1
+
+        return -1
