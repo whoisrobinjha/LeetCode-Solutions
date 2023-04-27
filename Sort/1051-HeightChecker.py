2097838ee12f9ba8c6fe4d3f12759f16ad55f771
whoisrobinjha <robinjha370@gmail.com>
2023-04-27 11:59:32 +0530

    Solutions modified

--- Sort/1051-HeightChecker.py
+++ Sort/1051-HeightChecker.py
@@ -11,13 +11,13 @@ class Solution:
         return count
     
     def quick_sort(self, arr):
-            if len(arr) <= 1:
-                return arr
-            else:
-                pivot = arr[0]
-                left = [x for x in arr[1:] if x <= pivot]
-                right = [x for x in arr[1:] if x > pivot]
-                return self.quick_sort(left) + [pivot] + self.quick_sort(right)
-
+        if len(arr) <= 1:
+            return arr
+        else:
+            pivot = arr[0]
+            left = [x for x in arr[1:] if x <= pivot]
+            right = [x for x in arr[1:] if x > pivot]
+            return self.quick_sort(left) + [pivot] + self.quick_sort(right)
+    
 heights = [1,1,4,2,1,3]
 print(Solution().heightChecker(heights))
