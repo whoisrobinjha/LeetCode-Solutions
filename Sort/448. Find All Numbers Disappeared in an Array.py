ce7ee205db229d31bef5d526ededd9669c21d535
whoisrobinjha <robinjha370@gmail.com>
2023-05-07 14:45:06 +0530

    modified 448 solution using cyclic sort algorithem

--- Sort/448. Find All Numbers Disappeared in an Array.py
+++ Sort/448. Find All Numbers Disappeared in an Array.py
@@ -2,18 +2,22 @@
 # https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
 
 class Solution:
-    def findDisappearedNumbers(self, nums):
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        for i in range(n):
-            val = abs(nums[i]) - 1
-            if nums[val] > 0:
-                nums[val] = -nums[val]
+        i = 0
+        
+        while i < n:
+            if nums[i] != nums[nums[i] - 1]:
+                j = nums[i] - 1
+                nums[i], nums[j] = nums[j], nums[i]
+            else:
+                i += 1
 
         missing_numbers = []
         for i in range(n):
-            if nums[i] > 0:
+            if nums[i] != i + 1:
                 missing_numbers.append(i + 1)
-            
+        
         return missing_numbers
 
 nums = [4,3,2,7,8,2,3,1]
