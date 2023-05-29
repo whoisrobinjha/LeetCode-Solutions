d26dd9ffb43fc1f21103545953936da5116fc63f
whoisrobinjha <robinjha370@gmail.com>
2023-05-29 10:26:46 +0530

    solution to problem 709 added

---
+++ Strings/709. To Lower Case.py
@@ -0,0 +1,9 @@
+#LeetCode Problem 709. To Lower Case Solution
+# https://leetcode.com/problems/to-lower-case/
+
+class Solution:
+    def toLowerCase(self, s):
+        return s.lower()
+    
+s = "Hello"
+print(Solution().toLowerCase(s))
